f565bd7bf463a5884fb40dd2cfd0f0ab841facdf
david.lee <davidmlee8@gmail.com>
2019-06-09 16:35:14 -0700

    new files added

---
+++ app/src/main/java/com/davidmlee/nytimes100/mvp_model/Contants.java
@@ -0,0 +1,13 @@
+package com.davidmlee.nytimes100.mvp_model;
+
+/**
+ * class Contants
+ *    collection of application contants
+ */
+
+public class Contants {
+    static final public String BASE_URL = "https://api.nytimes.com/svc/search/v2/";
+    static final public String API_KEY = "OKsEwghCzAPR3kRr7Hp51cFn2tMfXWgj";
+    static final public String BASE_IMAGE_URL = "https://graphics8.nytimes.com/";
+    static final public int NUM_ARTICLES_PER_PAGE = 10;
+}
